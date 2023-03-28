ef45d33e7e095f869cf3a378ac44be2652bcf7b9
Duha Emir Ganioğlu <duhaemirganioglu@TF001592.local>
2023-03-28 23:30:02 +0300

    Added the value and post into the login

--- frontend/src/api/authentication.js
+++ frontend/src/api/authentication.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const url = 'http://127.0.0.1:8000/';
 
 export const fetchLogin = (userData) => {
-    return axios.post(`${url}login`, userData);
+    console.log(userData);
+    return axios.post(`${url}/login`, userData);
 };
 
 export const fetchLocation = async () => {
