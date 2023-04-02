94f8425c1170e1f396bd5bae9f59052f3104c23c
Ece Pınar Özer <ecepinarozer@Eces-MacBook-Pro.local>
2023-04-02 18:28:54 +0300

    deployed mysql database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,11 +76,22 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'maindb',
+        'USER': 'admin',
+        'PASSWORD': 'password',
+        'HOST': 'maindb.cghqybkpxgzw.eu-north-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
