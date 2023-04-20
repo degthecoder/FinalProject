6fcb33e866e852354e7760ea707088e0c7b1f5ca
YIGIT YAKAR <yyakar19@ku.edu.tr>
2023-04-20 17:51:19 +0300

    User preferences to database after signup

--- backend/customer/views.py
+++ backend/customer/views.py
@@ -4,9 +4,15 @@ from django.http import JsonResponse
 import requests
 import json
 from rest_framework.decorators import api_view
+from backend.constants import get_user_id
+from customer.models import Customer
 # Create your views here.
 @api_view(['POST'])
 def insert_preferences(request):
-
     data = request.data
+    this_customer = Customer(user_customer_id=get_user_id(),cuisine_preference =list(data.keys()))
+    this_customer.save()
+    
+
+
     return JsonResponse(list(data.keys()),safe=False)
