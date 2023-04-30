71f10014904ecee58731571e53745fb61eda687d
Duha Emir Ganioğlu <emirganioglu@gmail.com>
2023-04-30 17:40:14 +0300

    Added the userlocation on the userNavbar for design purposes

--- frontend/src/themes/theme.jsx
+++ frontend/src/themes/theme.jsx
@@ -1,33 +1,33 @@
 import { createTheme } from "@material-ui/core";
 
 const theme = createTheme({
-  typography: {
-    fontFamily: ["Fira Sans", "sans-serif"].join(","),
-  },
-  palette: {
-    primary: {
-      main: "#780000",
-      transparent: "rgba(120, 0, 0,0.8)"
+    typography: {
+        fontFamily: ["Fira Sans", "sans-serif"].join(",")
     },
-    secondary: {
-      main: "#C1121F",
-    },
-    pink: {
-      main: "#FFB4C4",
-    },
-    green: {
-      main: "#405D12",
-    },
-    beige: {
-      main: "#FDF0D5",
-    },
-    darkBlue: {
-      main: "#003049",
-    },
-    blue: {
-      main: "#669BBC",
-    },
-  },
+    palette: {
+        primary: {
+            main: "#780000",
+            transparent: "rgba(120, 0, 0,0.8)"
+        },
+        secondary: {
+            main: "#C1121F"
+        },
+        pink: {
+            main: "#FFB4C4"
+        },
+        green: {
+            main: "#405D12"
+        },
+        beige: {
+            main: "#FDF0D5"
+        },
+        darkBlue: {
+            main: "#003049"
+        },
+        blue: {
+            main: "#669BBC"
+        }
+    }
 });
 
 export default theme;
