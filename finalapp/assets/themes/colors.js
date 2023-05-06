06a9abd68c723ffbbd82ca092c9fd20d1f2d4123
Duha Emir Ganioğlu <emirganioglu@gmail.com>
2023-05-07 01:03:21 +0300

    Initialized finalapp

---
+++ finalapp/assets/themes/colors.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native';
+
+const theme = StyleSheet.create({
+  typography: {
+    fontFamily: ['Fira Sans', 'sans-serif'].join(','),
+  },
+  palette: {
+    primary: {
+      main: '#780000',
+      transparent: 'rgba(120, 0, 0,0.8)',
+    },
+    secondary: {
+      main: '#C1121F',
+    },
+    pink: {
+      main: '#FFB4C4',
+    },
+    green: {
+      main: '#405D12',
+    },
+    beige: {
+      main: '#FDF0D5',
+    },
+    darkBlue: {
+      main: '#003049',
+    },
+    blue: {
+      main: '#669BBC',
+    },
+  },
+});
+
+export default theme;
